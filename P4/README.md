ac14eafe0eecb1ccdfaabdb3d7d6bd67a3474218
gacastroo <ga.castro.2019@alumnos.urjc.es>
2023-04-25 10:18:35 +0200

    cc

--- P4/README.md
+++ P4/README.md
@@ -1,1 +1,67 @@
  # Práctica 4
+## Memory
+
+
+1. Construir un juego de memoria además de aumentar tus conocimientos y mejorar tus habilidades con JavaScript, también puede ser bueno para tu memoria.
+
+2. En esta cuarta práctica, mostraremos un tablero de tarjetas, cada una de las tarjetas se puede voltear y emparejar.
+
+3. Si la pareja es correcta, hemos encontrado un match, y las cartas se seguirán mostrando.
+
+4. Pero si no es una combinación correcta, se vuelven a ocultar.
+
+5. El juego continúa hasta encontrar todas las parejas o reiniciar.
+
+
+
+
+### Elementos que debe tener el juego
+
+
+    * Botón para poner en marcha el juego.
+    * Botón de reinicio del juego.
+    * Un selector de posibles dimensiones del tablero con valores 2, 4, 6.
+    * Un display que mostrará el número de movimientos total que hemos hecho, y el tiempo que ha pasado desde que ha empezado la partida.
+    * Un tablero de tarjetas que puede tener las dimensiones 2, 4 o 6.
+
+
+### Funcionamiento
+
+
+Antes de iniciar el juego, podremos elegir las dimensiones del tablero.
+Comenzar
+
+Inicia el juego con el contador de movimientos y timer a cero.
+Reiniciar
+
+Nos permite reiniciar el juego al final de la partida sin necesidad de actualizar la página, o en medio de una partida.
+Dimensiones
+
+Nos permite seleccionar el número de dimensiones antes de comentar el juego, 2, 4 o 6. Y no podrá cambiar una vez el juego ha comenzado.
+Movimientos
+
+Acumula el número de movimientos necesario para resolver el tablero.
+
+#### Timer
+
+Acumula el tiempo transcurrido desde que damos al botón de comenzar o pulsamos una carta.
+
+#### Carta
+
+Si no se ha iniciado el juego, se iniciará al pulsar en cualquier carta.
+
+Además, dará la vuelta a la carta que hemos seleccionado hasta un máximo de dos.
+
+En ese momento comprobará si las dos cartas que hemos seleccionado son pareja.
+
+#### Caso de acierto
+
+Si hemos acertado las cartas se quedarán boca arriba.
+#### Caso de fallo
+
+Las cartas seleccionadas se vuelven a voltear.
+Cuando encontramos todas las parejas
+
+Mostraremos un mensaje al jugador indicando el número de movimientos que ha necesitado y el tiempo que ha transcurrido.
+
+En ese momento podremos volver a comenzar el juego.
---
+++ P5/Net/assets/javascript/net.js
@@ -0,0 +1,161 @@
+console.log("Montando la red...")
+
+const gui = {
+  bsend : document.getElementById("bsend"),
+  netdelay : document.getElementById("netdelay"),
+  nodos : document.getElementById("nodos"),
+  netdelayvalue : document.getElementById("netdelay_value"),
+  nodosvalue : document.getElementById("nodos_value"),
+
+}
+
+//-- Obtener elementos del DOM
+const canvas = document.getElementById('canvas');
+const imgBack = document.getElementById('imagesrchide');
+const imgCloud = document.getElementById('cloud');
+const imgFront = document.getElementById('imagesrc');
+const ctx = canvas.getContext('2d');
+
+//-- Gestionar el estado el envío
+const state = {
+  sendingImage: false,
+  totalTime: 0,
+  totalPackages:0,
+  sendingPackage:0,
+  netDelay: 1,
+  netDelayDefault: 1,
+  nodos: 2,
+  nodosDefault: 2,
+  loop: null
+}
+
+//-- Iniciar el valor del deslizador con el valor de la 
+// variable de estado para el delay
+gui.netdelayvalue.innerHTML = state.netDelay;
+gui.nodosvalue.innerHTML = state.nodosDefault;
+
+//-- Cuando está disponible cargo la imagen con la nube para represntar el destino
+imgCloud.onload = function () {
+
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
+  canvas.width = imgCloud.width;
+  canvas.height = imgCloud.height;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavía
+  ctx.drawImage(imgCloud, 0, 0);
+
+}
+
+//-- función de callback para el envío de la imagen
+gui.bsend.onclick = () => {
+  sendImage()
+}
+
+//-- función de callback para actualizar los valores del 
+// deslizador y la variable de estado para el delay
+gui.netdelay.oninput = () => {
+  gui.netdelayvalue.innerHTML = gui.netdelay.value;
+  state.netDelay = gui.netdelay.value;
+}
+
+gui.nodos.oninput = () => {
+  gui.nodosvalue.innerHTML = gui.nodos.value;
+  state.netDelay = gui.nodos.value;
+}
+
+
+
+//-- simulación del envío de la imagen
+//-- la he planteado como que cada línea horizontal de la imagen
+//-- es un paquete de datos, que sufrirá el retardo correspondiente.
+//-- https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
+const sendImage = () => {
+
+  console.log("Comienzo a enviar...");
+
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
+  canvas.width = imgFront.width;
+  canvas.height = imgFront.height;  
+ 
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavía
+  ctx.drawImage(imgFront, 0, 0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  //let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  state.totalPackages = canvas.height;
+  state.sendingImage = true;
+
+  //-- declaro las variables fuera del loop
+  //-- esta me servirá para seleccionar el rectángulo de la imagen
+  let imgData = ctx.getImageData(0, 0, 1, 1)
+  
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  state.loop = setInterval(() => {
+
+    state.totalTime++
+    state.sendingPackage++
+
+    //-- dimensiones del rectángulo 1
+    sx1 = 0;
+    sy1 = 0;
+    sw1 = canvas.width;
+    sh1 = state.sendingPackage;
+
+    imgData = ctx.getImageData(sx1, sy1, sw1, sh1);
+
+    //-- Obtener el array con todos los píxeles
+    data = imgData.data
+
+    //-- cambiamos el canal a rojo del rectángulo que hemos seleccionado
+    for (let i = 0; i < data.length; i+=4) {
+      data[i] = 0; //-- Canal rojo a 0
+    }
+    
+    //-- dimensiones del rectángulo 2
+    sx2 = sx1;
+    sy2 = sh1+1;
+    sw2 = sw1;
+    sh2 = state.totalPackages - sh1;
+
+    // if (sh2 > 0) {
+    //   //-- seleccionamos el rectángulo 2
+    //   imgData = ctx.getImageData(sx2, sy2, sw2, sh2);
+
+    //   //-- Obtener el array con todos los píxeles
+    //   data = imgData.data
+  
+    //   for (let i = 0; i < data.length; i+=4) {
+    //     // brillo = (3 * r + 4 * g + b)/8
+    //     //let brillo = (3 * data[i] + 4 * data[i+1] + data[i+2])/8
+    //     //data[i] = brillo;
+    //     //data[i+1] = brillo;
+    //     //data[i+2] = brillo; 
+    //     //data[i] = 0;
+    //     //data[i+1] = 0;
+    //     data[i+2] = 0;         
+    //   }  
+    // }
+
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+
+    // Paramos el loop si hemos terminado de enviar
+    if (state.sendingPackage == state.totalPackages) {
+      ctx.drawImage(imgBack, 0, 0); 
+      console.log("Envio terminado...");
+      state.sendingImage = false;             
+      clearInterval(state.loop);
+    }
+
+    console.log("Enviando...");
+  }, state.netDelay)
+}
+
+console.log("Red preparada...");
